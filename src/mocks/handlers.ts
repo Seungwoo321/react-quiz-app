3d9000ddab8a3be7a9717172121255ddf7d70f8f
Seungwoo321 <seungwoo321@gmail.com>
2023-12-08 13:51:54 +0900

    fix: update amount

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,7 +7,7 @@ const fetchQuizQuestions = <T extends { request: any, params: any, cookies: any}
   const amount = Number(url.searchParams.get('amount') || 0)
   const difficulty= url.searchParams.get('difficulty')
   const type = url.searchParams.get('type')
-  const results = shuffleArray(questions).filter(question => question.difficulty === difficulty && question.type === type).slice(0, amount - 1)
+  const results = shuffleArray(questions).filter(question => question.difficulty === difficulty && question.type === type).slice(0, amount)
   return HttpResponse.json({
     results
   })
